85842d0c10613ccd9e1d579ad1fb223f07f6c89d
ryunosuke-noyama <ryu1998.08.21@icloud.com>
2021-01-23 19:21:15 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :cofigure_permitted_parameters, if: :devise_controller?
+
+  def cofigure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
